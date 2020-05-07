5550981525d39d4e76dcbbae3e4b7d6bad529947
Nitsan Avni <nitsan@papayaglobal.com>
2020-05-07 09:41:36 +0300

    wip plop

--- plop/plopfile.ts
+++ plop/plopfile.ts
@@ -1,6 +1,20 @@
 import { NodePlopAPI } from "plop";
+import execa from "execa";
+import getStream from "get-stream";
 
 export default function (plop: NodePlopAPI) {
+	const path = "test/{{name}}.spec.{{ts}}";
+
+	plop.setActionType("git add", async (answers: any, _, p) => {
+		const filePath = p?.renderString(path, answers)!;
+
+		await execa("git", ["add", filePath]);
+
+		const { stdout } = await execa("git", ["status", "-s", filePath]);
+
+		return stdout;
+	});
+
 	plop.setGenerator("test", {
 		description: "a test",
 		prompts: [
@@ -8,10 +22,13 @@ export default function (plop: NodePlopAPI) {
 			{ type: "list", choices: ["ts", "tsx"], name: "ts" },
 		],
 		actions: [
-			(ans: any) => ((ans.isReact = ans.ts === "tsx"), ""),
+			(ans: any) => (
+				(ans.isReact = ans.ts === "tsx"),
+				ans.isReact ? "it's a react test, got it" : "not a react test"
+			),
 			{
 				type: "add",
-				path: "test/{{name}}.spec.{{ts}}",
+				path,
 				template: `{{#if isReact}}import React from "react";
 {{/if}}
 import test from "ava";
@@ -19,6 +36,7 @@ import test from "ava";
 test("", (t) => {});
 `,
 			},
+			{ type: "git add" },
 		],
 	});
 }
