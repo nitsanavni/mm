6485cb438bd9016de23fb7e9ee82b303638d2e10
Nitsan Avni <nitsan@papayaglobal.com>
2020-05-07 08:57:06 +0300

    wip plop

--- plop/plopfile.ts
+++ plop/plopfile.ts
@@ -3,7 +3,22 @@ import { NodePlopAPI } from "plop";
 export default function (plop: NodePlopAPI) {
 	plop.setGenerator("test", {
 		description: "a test",
-		prompts: [],
-		actions: [],
+		prompts: [
+			{ message: "name", name: "name" },
+			{ type: "list", choices: ["ts", "tsx"], name: "ts" },
+		],
+		actions: [
+			(ans: any) => ((ans.isReact = ans.ts === "tsx"), ""),
+			{
+				type: "add",
+				path: "test/{{name}}.spec.{{ts}}",
+				template: `{{#if isReact}}import React from "react";
+{{/if}}
+import test from "ava";
+
+test("", (t) => {});
+`,
+			},
+		],
 	});
 }
--- test/table.spec.tsx
+++ test/table.spec.tsx
@@ -1,3 +1,3 @@
 import test from "ava";
 
-test("");
+test("single", (t) => {});
