d1c2e08d2a62f23dbbdf74c5929686240f6ca21c
Nitsan Avni <nitsan@papayaglobal.com>
2020-05-08 09:58:44 +0300

    fix: restore view

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { find } from "lodash";
 
 import { Outline } from "./outline.component";
-import { Player } from "./demo";
-import { clips } from "./clips";
 
-export const App = ({ file, help }: { file?: string; help: boolean }) => (
-	<>
-		<Player clip={find(clips, { name: "move up" })!.clip} />
-		<Outline file={file} />
-	</>
+export const App = ({ file }: { file?: string; help: boolean }) => (
+	<Outline file={file} />
 );
