f8c66caa2d9a4f458183714d8c958455ef40e813
Nitsan Avni <nitsan@papayaglobal.com>
2020-05-11 19:22:27 +0300

    docs: update todos

--- src/table/table.ts
+++ src/table/table.ts
@@ -22,15 +22,12 @@ export type Table = {
 // - text representation for the saved file - to/from plain text, indent like the other nodes, trim values
 // - extract key adapter: {... (alt + up arrow): moveRowUp, (up arrow): up ...}
 // - addColumn & grab it to move
-// - grab column mode -> now left/right arrows move the column
 // - addColumn = addColumnRightOf(focus)
 // - addColumnLeftOf(focus)
 // - addRow = addRowBelow(focus) = each(columns, column => addCellBelow(column, focus.row))
 // - addRowAbove(focus)
-// - delete = deleteCellValue
-// - deleteRow
-// - deleteColumn
 // not mvp:
+// - grab column mode -> now left/right arrows move the column
 // - transpose!
 // - collapse
 //   - keep only first cell
